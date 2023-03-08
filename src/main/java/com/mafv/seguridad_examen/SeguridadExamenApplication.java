cba1944a5ecfc7ca153584cb572b3667810f4fef
Miguel Angel Femenia <mfemvaz2906@g.educaand.es>
2023-03-08 10:00:04 +0100

    first commit

---
+++ src/main/java/com/mafv/seguridad_examen/SeguridadExamenApplication.java
@@ -0,0 +1,13 @@
+package com.mafv.seguridad_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadExamenApplication.class, args);
+	}
+
+}
